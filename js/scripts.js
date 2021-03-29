c96eabf07d3062460ae2ae88470249f883329cb2
James Wyn <thisisjameswyn@gmail.com>
2021-03-29 06:48:55 -0700

    add JS question form element

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-
+  $("form#welcomeSelect").submit(function(event) {
+    const userName = $("input#name").val();
+    const op1 = parseInt($("input:radio[name=question1]:checked").val());
+    const op2 = parseInt($("input:radio[name=question2]:checked").val());
+    const op3 = parseInt($("input:radio[name=question3]:checked").val());
+    const op4 = parseInt($("input:radio[name=question4]:checked").val());
+    const op5 = parseInt($("input:radio[name=question5]:checked").val());
+    const total = op1+op2+op3+op4+op5;
+  });
 });
