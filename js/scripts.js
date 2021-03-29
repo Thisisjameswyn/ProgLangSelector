02b65cbc8a4e9d88bb291c1207fb4449e41f41a2
James Wyn <thisisjameswyn@gmail.com>
2021-03-29 07:03:02 -0700

    add project art assets

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,30 @@ $(document).ready(function() {
     const op4 = parseInt($("input:radio[name=question4]:checked").val());
     const op5 = parseInt($("input:radio[name=question5]:checked").val());
     const total = op1+op2+op3+op4+op5;
+
+    $(".name").text(userName);
+
+    if(total >= 9) {
+      $(".welcome").hide();
+      $(".questions").hide();
+      $("#answer1").toggle("slow");
+    }else if(total <= 8 && total > 6) {
+      $(".welcome").hide();
+      $(".questions").hide();
+      $("#answer2").toggle("slow");
+    }else if(total < 6) {
+      $(".welcome").hide();
+      $(".questions").hide();
+      $("#answer3").toggle("slow");
+    }else {
+      $(".magicWord").toggle("slow");
+      $(".welcome").hide();
+      $(".questions").hide();
+    }
+
+    event.preventDefault();
+  });
+  $("form#reset").submit(function(event) {
+    location.reload();
   });
 });
